7913f2628909faf9ab1a77e3257dca5679bd4b69
Ronaldo Castilho <ronaldolcastilho@gmail.com>
2023-02-28 16:27:33 -0300

    create project

---
+++ src/test/java/com/restwithspring/RestWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
